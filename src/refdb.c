65bb23c7e0aa27df13a963677b46cb867e37d6c1
Patrick Steinhardt <ps@pks.im>
2020-06-30 10:13:26 +0200

    refdb: avoid unlimited spinning in case of symref cycles
    To determine whether another reflog entry needs to be written for HEAD
    on a reference update, we need to see whether HEAD directly or
    indirectly points to the reference we're updating. The resolve logic is
    currently completely unbounded except an error occurs, which effectively
    means that we'd be spinning forever in case we have a symref loop in the
    repository refdb.
    
    Let's fix the issue by using `git_refdb_resolve` instead, which is
    always bounded.

--- src/refdb.c
+++ src/refdb.c
@@ -325,7 +325,7 @@ int git_refdb_should_write_reflog(int *out, git_refdb *db, const git_reference *
 
 int git_refdb_should_write_head_reflog(int *out, git_refdb *db, const git_reference *ref)
 {
-	git_reference *head = NULL, *peeled = NULL;
+	git_reference *head = NULL, *resolved = NULL;
 	const char *name;
 	int error;
 
@@ -343,22 +343,13 @@ int git_refdb_should_write_head_reflog(int *out, git_refdb *db, const git_refere
 		goto out;
 
 	/* Go down the symref chain until we find the branch */
-	while (git_reference_type(head) == GIT_REFERENCE_SYMBOLIC) {
-		if ((error = git_refdb_lookup(&peeled, db, git_reference_symbolic_target(head))) < 0)
-			break;
-
-		git_reference_free(head);
-		head = peeled;
-		peeled = NULL;
-	}
-
-	if (error < 0) {
+	if ((error = git_refdb_resolve(&resolved, db, git_reference_symbolic_target(head), -1)) < 0) {
 		if (error != GIT_ENOTFOUND)
 			goto out;
 		error = 0;
-		name = git_reference_symbolic_target(head);
+		name = git_reference_symbolic_target(resolved ? resolved : head);
 	} else {
-		name = git_reference_name(head);
+		name = git_reference_name(resolved);
 	}
 
 	if (strcmp(name, ref->name))
@@ -367,7 +358,7 @@ int git_refdb_should_write_head_reflog(int *out, git_refdb *db, const git_refere
 	*out = 1;
 
 out:
-	git_reference_free(peeled);
+	git_reference_free(resolved);
 	git_reference_free(head);
 	return error;
 }
